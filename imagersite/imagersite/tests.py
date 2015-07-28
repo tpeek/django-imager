1b5f1be78cfe3da38cfebf62082876fbea30ccd8
Jason Tyler <jmtyler@gmail.com>
2015-07-28 14:20:43 -0700

    working on tests; pulling for shared test media

---
+++ imagersite/imagersite/tests.py
@@ -0,0 +1,50 @@
+import unittest
+from django.test import Client
+from imager_images.models import Photo
+from imager_profile.models import User
+import factory
+from faker import Faker
+
+
+fake = Faker()
+
+
+class PhotoFactory(factory.Factory):
+    class Meta:
+        model = Photo
+    file = fake.file()
+    title = fake.title()
+    description = fake.description()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = factory.LazyAttribute(lambda a: '{}.{}@example.com'.format(
+                                  a.first_name, a.last_name).lower())
+    username = factory.LazyAttribute(lambda a: '{}{}{}{}'.format(
+                                     a.last_name[:1], a.first_name[1:],
+                                     a.first_name[:1], a.last_name[1:]).lower())
+
+
+class HomeExists(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_self_exists(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+class RandomPhotos(unittest.TestCase):
+    def setUp(self):
+        owner = UserFactory.create()
+        owner.save()
+        self.photo1 = PhotoFactory.create(owner=owner, privacy='PU') 
+
+    def test_random_photo_in_site(self):
+
+        response = self.client.get('/')
+        print response
