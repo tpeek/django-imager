057defb321451b8485d5282774350f0f2598282c
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-07-31 16:57:46 -0700

    add album, no logic

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from imager_images.models import Photo, Album
 from django.contrib.auth.decorators import login_required
+from .forms import *
 
 
 @login_required
@@ -22,7 +23,12 @@ def library_view(request):
 
 @login_required
 def add_album_view(request):
-    return render(request, 'add_album.html')
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        return render(request, 'add_album.html', {'form': form.as_p})
+    else:
+        form = AlbumForm()
+        return render(request, 'add_album.html', {'form': form.as_p})
 
 
 @login_required
