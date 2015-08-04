3dae13db9d1b1b76baef038f55d7774c85bb81b4
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-03 18:22:05 -0700

    postgres

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -143,8 +143,12 @@ BOOTSTRAP3 = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-imager',
+        'USER': 'admin',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
