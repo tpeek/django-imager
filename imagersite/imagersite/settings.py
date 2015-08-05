21e654f5f4f1f18497481705e9225ef68a7f5fef
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-04 18:37:16 -0700

    update db-url

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -145,15 +146,9 @@ BOOTSTRAP3 = {
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'django-imager',
-        'USER': 'admin',
-        'PASSWORD': 'pass',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config()
 }
 
 CACHES = {
