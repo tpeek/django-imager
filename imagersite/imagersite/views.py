cb70dfc87322f1e6e8883b5ddd4db5296ea10c33
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-05 19:10:31 -0700

    fix static link on home for real this time

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -7,7 +7,7 @@ def home_view(request):
         pic_url = Photo.objects.filter(
             privacy='Public').order_by('?').first().file
     except AttributeError:
-        pic_url = 'demo.jpg'
+        pic_url = ''
     return render(request, 'home.html', {'pic_url': pic_url})
 
 
