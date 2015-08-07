1d17744b37624405211121a2214aa84f04c9d7e0
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-08-06 20:42:45 -0700

    email

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -183,7 +183,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if os.environ['EMAIL_BACKEND']:
+    EMAIL_BACKEND = os.environ['EMAIL_BACKEND']
 
 # For django-registration-redux
 ACCOUNT_ACTIVATION_DAYS = 3
